94363f2ada37204a8dd779d72747766afc553b10
Aitor Cuñado <aitorcuga@gmail.com>
2026-07-23 16:32:51 +0200

    Corregir guardado del formulario de personaje

--- scripts/sheets/character-sheet.js
+++ scripts/sheets/character-sheet.js
@@ -6,12 +6,15 @@ export class CharacterSheet extends HandlebarsApplicationMixin(ActorSheetV2) {
   static DEFAULT_OPTIONS = {
     classes: ["mythras-foundry", "actor-sheet", "character-sheet"],
 
+    tag: "form",
+
     position: {
       width: 600,
       height: 400
     },
 
     form: {
+      handler: CharacterSheet._onSubmitForm,
       closeOnSubmit: false,
       submitOnChange: true
     }
@@ -22,6 +25,10 @@ export class CharacterSheet extends HandlebarsApplicationMixin(ActorSheetV2) {
       template: "systems/mythras-foundry/templates/actor/character-sheet.hbs"
     }
   };
+
+  static async _onSubmitForm(event, form, formData) {
+    await this.actor.update(formData.object);
+  }
 
   async _prepareContext(options) {
     const context = await super._prepareContext(options);
