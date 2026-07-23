88ea29e094c4c6d515c75a9611a2e70020b62516
Aitor Cuñado <aitorcuga@gmail.com>
2026-07-23 16:22:59 +0200

    Mostrar y editar la característica Fuerza

--- scripts/sheets/character-sheet.js
+++ scripts/sheets/character-sheet.js
@@ -3,19 +3,32 @@ const { HandlebarsApplicationMixin } = foundry.applications.api;
 
 export class CharacterSheet extends HandlebarsApplicationMixin(ActorSheetV2) {
 
-    static DEFAULT_OPTIONS = {
+  static DEFAULT_OPTIONS = {
     classes: ["mythras-foundry", "actor-sheet", "character-sheet"],
+
     position: {
       width: 600,
       height: 400
-      }
-    };
+    },
+
+    form: {
+      closeOnSubmit: false,
+      submitOnChange: true
+    }
+  };
 
   static PARTS = {
     main: {
       template: "systems/mythras-foundry/templates/actor/character-sheet.hbs"
-      }
-    };
+    }
+  };
 
+  async _prepareContext(options) {
+    const context = await super._prepareContext(options);
+
+    context.actor = this.actor;
+
+    return context;
+  }
 
 }
