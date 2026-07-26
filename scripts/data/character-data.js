fd3b23a4661b9044a2dd0f4f5919a22a283eb599
Aitor Cuñado <aitorcuga@gmail.com>
2026-07-26 21:12:14 +0200

    Añadir características y estado de generación

--- scripts/data/character-data.js
+++ scripts/data/character-data.js
@@ -1,10 +1,64 @@
-const { NumberField } = foundry.data.fields;
+const { NumberField, BooleanField } = foundry.data.fields;
 
 export class CharacterData extends foundry.abstract.TypeDataModel {
 
   static defineSchema() {
     return {
+      characteristicsGenerated: new BooleanField({
+        required: true,
+        nullable: false,
+        initial: falseva
+      }),
+
       strength: new NumberField({
+        required: true,
+        nullable: false,
+        integer: true,
+        initial: 10,
+        min: 1
+      }),
+
+      constitution: new NumberField({
+        required: true,
+        nullable: false,
+        integer: true,
+        initial: 10,
+        min: 1
+      }),
+
+      size: new NumberField({
+        required: true,
+        nullable: false,
+        integer: true,
+        initial: 10,
+        min: 1
+      }),
+
+      dexterity: new NumberField({
+        required: true,
+        nullable: false,
+        integer: true,
+        initial: 10,
+        min: 1
+      }),
+
+      intelligence: new NumberField({
+        required: true,
+        nullable: false,
+        integer: true,
+        initial: 10,
+        min: 1
+      }),
+
+      power: new NumberField({
+        required: true,
+        nullable: false,
+        integer: true,
+        initial: 10,
+        min: 1
+      }),
+
+      charisma: new NumberField({
         required: true,
         nullable: false,
         integer: true,
